50ca9d726cf85bcfccab24a10de286b3dcad0eeb
jinfayap <jinfayap@gmail.com>
2022-03-04 16:17:58 +0800

    Fix typo in controller

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -26,7 +26,7 @@ class AdminController extends Controller
         return view('admin.role-permission');
     }
 
-    public function useRolePermission()
+    public function userRolePermission()
     {
         return view('admin.user-role-permission');
     }
